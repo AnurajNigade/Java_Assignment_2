59653394d0210ff1234d71168e56492161463771
Anuraj Nigade <96386849+AnurajNigade@users.noreply.github.com>
2023-10-01 18:53:33 +0530

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,87 +1,85 @@
-//package com.dbda.main;
-//class Book{
-//	
-//}
-//public class Library {
-//
-//	  private List<Book> books;
-//
-//	  public Library() {
-//	    books = new ArrayList<Book>();
-//	  }
-//
-//	  public void addBook(Book book) {
-//	    books.add(book);
-//	  }
-//
-//	  public Book checkOutBook(String title) {
-//	     for (Book book : books) {
-//	      if (book.getTitle().equals(title)) {
-//	        book.setAvailable(false);
-//	        return book;
-//	      }
-//	    }
-//	    return null;
-//	  }
-//
-//	  public Book returnBook(Book book) {
-//	    book.setAvailable(true);
-//	    return book;
-//	  }
-//
-//	} 
-//	Example
-//	Library library = new Library();
-//
-//	Book book1 = new Book("The Lord of the Rings", "J.R.R. Tolkien");
-//	Book book2 = new Book("The Hobbit", "J.R.R. Tolkien");
-//
-//	library.addBook(book1);
-//	library.addBook(book2);
-//
-//	Book checkedOutBook = library.checkOutBook("The Lord of the Rings");
-//	System.out.println("Checked out book: " + checkedOutBook.getTitle());
-//
-//	library.returnBook(checkedOutBook);
-//	System.out.println("Book returned: " + checkedOutBook.getTitle());
-//	}
-//}
-//
-//public class Book {
-//
-//	  private String title;
-//	  private String author;
-//	  private boolean isAvailable;
-//
-//	  public Book(String title, String author) {
-//	    this.title = title;
-//	    this.author = author;
-//	    this.isAvailable = true;
-//	  }
-//
-//	  public String getTitle() {
-//	    return title;
-//	  }
-//
-//	  public String getAuthor() {
-//	    return author;
-//	  }
-//
-//	  public boolean isAvailable() {
-//	    return isAvailable;
-//	  }
-//
-//	  public void setAvailable(boolean available) {
-//	    isAvailable = available;
-//	  }
-//
-//	} 
-//
-//public class Library {
-//	public static void main(String[] args) {
-//		
-//	}
-//		
-//			
-//			
-//}
+package com.dbda.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Book1 {
+
+	  private String title;
+	  private String author;
+	  private boolean isAvailable;
+
+	  public Book1(String title, String author) {
+	    this.title = title;
+	    this.author = author;
+	    this.isAvailable = true;
+	  }
+
+	  public String getTitle() {
+	    return title;
+	  }
+
+	  public String getAuthor() {
+	    return author;
+	  }
+
+	  public boolean isAvailable() {
+	    return isAvailable;
+	  }
+
+	  public void setAvailable(boolean available) {
+	    isAvailable = available;
+	  }
+
+	} 
+public class Library {
+
+	  private List<Book1> books;
+
+	  public Library() {
+	    books = new ArrayList<Book1>();
+	  }
+
+	  public void addBook(Book1 book) {
+	    books.add(book);
+	  }
+
+	  public Book1 checkOutBook(String title) {
+	     for (Book1 book : books) {
+	      if (book.getTitle().equals(title)) {
+	        book.setAvailable(false);
+	        return book;
+	      }
+	    }
+	    return null;
+	  }
+
+	  public Book1 returnBook(Book1 book) {
+	    book.setAvailable(true);
+	    return book;
+	  }
+
+	 
+	public static void main(String[] args) {
+	
+
+		Library library = new Library();
+	
+		Book1 book1 = new Book1("The Lord of the Rings", "J.R.R. Tolkien");
+		Book1 book2 = new Book1("The Hobbit", "J.R.R. Tolkien");
+	
+		library.addBook(book1);
+		library.addBook(book2);
+	
+		Book1 checkedOutBook = library.checkOutBook("The Lord of the Rings");
+		System.out.println("Checked out book: " + checkedOutBook.getTitle());
+	
+		library.returnBook(checkedOutBook);
+		System.out.println("Book returned: " + checkedOutBook.getTitle());
+	}
+}
+
+
+
+
+
